5c73998ba3c7c14f5ed363fba1d483422bae02e9
Yustinia <yustinia@protonmail.com>
2026-01-30 22:29:40 +0800

    add player & border collision

--- main.py
+++ main.py
@@ -13,6 +13,28 @@ class Environment:
         screen.blit(self.image, self.rect)
 
 
+class Player(pygame.sprite.Sprite):
+    def __init__(self, width: int, height: int, x: int, y: int, speed: int = 7) -> None:
+        super().__init__()
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+        self.speed = speed
+
+        self.image = pygame.image.load("assets/player.png")
+        self.rect = self.image.get_rect(center=(self.x, self.y))
+
+    def draw(self, screen):
+        screen.blit(self.image, self.rect)
+
+    def movement(self, keys):
+        if keys[pygame.K_LEFT] and self.rect.left >= 0:
+            self.rect.x -= self.speed
+        if keys[pygame.K_RIGHT] and self.rect.right <= self.width:
+            self.rect.x += self.speed
+
+
 class Game:
     def __init__(self, width: int, height: int) -> None:
         self.width = width
@@ -25,6 +47,11 @@ class Game:
 
         # instantiate objects
         self.environment = Environment(self.width, self.height)
+        self.player = Player(self.width, self.height, self.width // 2, self.height - 80)
+
+        # collision group for player & bullet
+        self.collision_group = pygame.sprite.Group()
+        self.collision_group.add(self.player)
 
     def event_handler(self):
         for event in pygame.event.get():
@@ -32,10 +59,12 @@ class Game:
                 self.is_running = False
 
     def update(self):
-        pass
+        keys = pygame.key.get_pressed()
+        self.player.movement(keys)
 
     def draw(self):
         self.environment.draw(self.screen)
+        self.collision_group.draw(self.screen)
 
     def run(self):
         while self.is_running:
